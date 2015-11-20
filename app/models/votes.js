a80520049f941ced0dd3f0708e83d8823899f931
stepang <g.stepanek.nz@gmail.com>
2015-11-20 07:59:28 +0000

    update

---
+++ app/models/votes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Votes = new Schema({
+    vote: { 
+        name: String, 
+        id: { type:Schema.ObjectId, default: mongoose.Types.ObjectId },
+        userid: String,
+        options: [ { 
+            name: String, 
+            count: Number, 
+            id: { type:Schema.ObjectId, default: mongoose.Types.ObjectId }
+        } ]
+    }
+})
+
+module.exports = mongoose.model('Votes', Votes);
